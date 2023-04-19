8826ce00a25496008ba81587484a7e75284b9292
jpfalcuci <jpfalcuci@gmail.com>
2023-04-18 21:12:35 -0300

    feat: Status de código de sucesso e correções

--- trabalho_api/index.js
+++ trabalho_api/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 const porta = 6660
 const arquivo = './tarefas.json';
-const encode = 'utf-8';
+const encoding = 'utf-8';
 
 app.listen(porta, () => {
     console.log(`O server está ativo em http://localhost:${porta}`);
@@ -21,11 +21,11 @@ app.listen(porta, () => {
 
 
 function lerJson(caminhoArquivo) {
-    if (!fs.existsSync(caminhoArquivo)) {               // verifica se o arquivo existe
-        fs.writeFileSync(caminhoArquivo, '[]', encode); // se não existir, cria um novo arquivo com um array vazio
+    if (!fs.existsSync(caminhoArquivo)) {                   // verifica se o arquivo existe
+        fs.writeFileSync(caminhoArquivo, '[]', encoding);   // se não existir, cria um novo arquivo com um array vazio
     }
     try {
-        const arquivo = fs.readFileSync(caminhoArquivo, encode);
+        const arquivo = fs.readFileSync(caminhoArquivo, encoding);
         const conteudo = JSON.parse(arquivo);
         return conteudo;
     } catch (err) {
@@ -42,10 +42,14 @@ function salvarJson(objeto, caminhoArquivo) {
     // MÉTODOS
 
 
-app.get('/', (req, res) => { 
+app.get('/tarefas', (req, res) => { 
     let tarefas = lerJson(arquivo);
+
     if (tarefas) {
-        res.json(tarefas);
+        res.status(200).json({
+            status: 'success',
+            data: tarefas
+        });
     } else {
         // Erro 500: Internal Server Error (Erro interno do servidor)
         res.status(500).json({ erro: "Erro ao ler arquivo" });
@@ -57,8 +61,11 @@ app.get('/tarefa/:id', (req, res) => {
     let tarefas = lerJson(arquivo);
     let tarefa = tarefas.find(tarefa => tarefa.id == req.params.id);    // encontra a tarefa que tem o ID informado
 
-    if (tarefa) {
-        res.json(tarefa);   // se encontrar, retorna a tarefa
+    if (tarefa) {   // se encontrar, retorna a tarefa
+        res.status(200).json({
+            status: 'success',
+            data: tarefa
+        });
     } else {
         // Erro 404: Not Found (Não encontrado)
         res.status(404).json({ erro: "Tarefa não encontrada" });
@@ -69,8 +76,8 @@ app.get('/tarefa/:id', (req, res) => {
 app.post('/criar-tarefa', (req, res) => {
     let tarefas = lerJson(arquivo);
 
-    // verifica se foi informado um "título" para a tarefa, e se o título não é vazio
-    if (req.body.titulo && req.body.titulo.trim() !== '') {
+    // verifica se foi informado um "título" para a tarefa, e se o título é uma string e não é vazia
+    if (req.body.titulo && typeof req.body.titulo === 'string' && req.body.titulo.trim() !== '') {
         // Encontra o maior ID existente
         let maiorId = 0;
         for (let i = 0; i < tarefas.length; i++) {
@@ -82,14 +89,18 @@ app.post('/criar-tarefa', (req, res) => {
         // cria um novo objeto com os valores recebidos e um novo ID baseado no maior ID existente
         let novaTarefa = {
             id: maiorId + 1,                        // maior id encontrado + 1
-            titulo: req.body.titulo,
+            titulo: req.body.titulo.trim(),
             descricao: req.body.descricao || '',    // se não for informada descrição, então será ''
-            feito: req.body.feito || false          // se não for informado o campo 'feito', então será false
+            // verifica se foi informado valor de 'feito' e se é boolean, se não, valor será 'false'
+            feito: typeof req.body.feito === 'boolean' ? req.body.feito : false
         };
     
         tarefas.push(novaTarefa);   // envia o novo objeto para o json
         salvarJson(tarefas, arquivo);
-        res.json(novaTarefa);
+        res.status(200).json({
+            status: 'success',
+            data: novaTarefa
+        });
     } else {
         // Erro 400: Bad Request (Requisição inválida)
         res.status(400).json({ mensagem: "O título da tarefa é obrigatório" });
@@ -99,23 +110,23 @@ app.post('/criar-tarefa', (req, res) => {
 
 app.put('/atualizar-tarefa/:id', (req, res) => {
     let tarefas = lerJson(arquivo);
-
-    // coleta a posição do item do ID informado
-    let index = tarefas.findIndex(tarefa => tarefa.id == req.params.id);
-
+    let index = tarefas.findIndex(tarefa => tarefa.id == req.params.id);    // coleta a posição do item do ID informado
+    
     if (index != -1) {
-        // mescla apenas os campos enviados na requisição
-        let tarefaAtualizada = {
-            // operador spread '...' cria uma cópia do item na posição 'index' do objeto 'tarefas'.
-            ...tarefas[index], 
-            titulo: req.body.titulo || tarefas[index].titulo,           // se não for informado título, mantém título existente
+        let tarefaAtualizada = {    // mescla apenas os campos enviados na requisição
+            ...tarefas[index],      // operador spread '...' cria uma cópia do item na posição 'index' do objeto 'tarefas'.
+            // verifica se foi informado título, se é string e se não é vazia, se não, mantém título existente
+            titulo: typeof req.body.titulo === 'string' && req.body.titulo.trim() !== '' ? req.body.titulo.trim() : tarefas[index].titulo,
             descricao: req.body.descricao || tarefas[index].descricao,  // se não for informada descrição, mantém descrição existente
-            feito: req.body.feito || tarefas[index].feito               // se não for informado o valor de 'feito', mantém o valor existente
+            // verifica se foi informado valor de 'feito' e se é boolean, se não, mantém o valor existente
+            feito: typeof req.body.feito === 'boolean' ? req.body.feito : tarefas[index].feito 
         };
-
-        tarefas[index] = tarefaAtualizada;                  // atualiza o item do json, usando o index
+        tarefas[index] = tarefaAtualizada;  // atualiza o item do json, usando o index
         salvarJson(tarefas, arquivo);
-        res.json(tarefaAtualizada);
+        res.status(200).json({
+            status: 'success',
+            data: tarefaAtualizada
+        });
     } else {
         // Erro 404: Not Found (Não encontrado)
         res.status(404).json({ erro: "Tarefa não encontrada" });
@@ -125,14 +136,16 @@ app.put('/atualizar-tarefa/:id', (req, res) => {
 
 app.delete('/deletar-tarefa/:id', (req, res) => {
     let tarefas = lerJson(arquivo);
-
-    // filtra selecionando todos os itens menos o item com o ID informado, e adiciona a um novo objeto
-    let tarefasAtualizadas = tarefas.filter(tarefa => tarefa.id != req.params.id);
+    let index = tarefas.findIndex(tarefa => tarefa.id == req.params.id);    // coleta a posição do item do ID informado
 
     // se o novo objeto for menor que o original, significa que um item foi removido
-    if (tarefasAtualizadas.length < tarefas.length) {
-        salvarJson(tarefasAtualizadas, arquivo);
-        res.json({ mensagem: "Tarefa excluída com sucesso" });
+    if (index !== -1) {
+        tarefas.splice(index, 1);   // remove o item correspondente ao index
+        salvarJson(tarefas, arquivo);
+        res.status(200).json({
+            status: 'success',
+            mensagem: `Tarefa com id ${req.params.id} excluída com sucesso`
+        });
     } else {
         // Erro 404: Not Found (Não encontrado)
         res.status(404).json({ erro: "Tarefa não encontrada" });
