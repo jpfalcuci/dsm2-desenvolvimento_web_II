453047cabefe72b936c048553f0ef0e232a5dd73
jpfalcuci <jpfalcuci@gmail.com>
2023-04-25 21:20:33 -0300

    fix: Correção de exibição das tarefas no html

--- trabalho_api/assets/js/main.js
+++ trabalho_api/assets/js/main.js
@@ -7,10 +7,10 @@ createApp({
         }
     },
     mounted() {
-        fetch('http://localhost:6660')
+        fetch('http://localhost:6660/tarefas/')
             .then(response => response.json())
             .then(data => {
-                this.tarefas = data
+                this.tarefas = data.data
                 console.log(this.tarefas)
             })
             .catch(error => console.log(error))
