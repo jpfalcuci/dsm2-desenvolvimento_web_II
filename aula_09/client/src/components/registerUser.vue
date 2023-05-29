fd53851520c65e2f68da20222886ebc52db3e747
jpfalcuci <jpfalcuci@gmail.com>
2023-05-29 20:14:20 -0300

    refactor: Melhorias no css

--- aula_09/client/src/components/registerUser.vue
+++ aula_09/client/src/components/registerUser.vue
@@ -6,17 +6,18 @@
       <input type="text" v-model="nome" placeholder="Digite seu nome" />
       <input type="email" v-model="email" placeholder="Digite seu email" />
       <input type="password" v-model="password" placeholder="Digite sua senha" />
-      <button type="submit">{{ editMode ? "Atualizar" : "Registrar" }}</button>
+      <button type="submit" :title='editMode ? "Atualizar" : "Registrar"'>{{ editMode ? "Atualizar" : "Registrar" }}</button>
     </form>
 
     <p>{{ message }}</p>
 
     <table v-if="users.length" class="user-list">
       <thead>
-        <tr>
+        <tr class="user-title">
           <th>Nome</th>
           <th>Email</th>
-          <th></th>
+          <th>Editar</th>
+          <th>Deletar</th>
         </tr>
       </thead>
       <tbody>
@@ -24,8 +25,14 @@
           <td>{{ user.nome }}</td>
           <td>{{ user.email }}</td>
           <td>
-            <button @click="editUser(user)" class="edit-button">Editar</button>
-            <button @click="deleteUser(user)" class="delete-button">Excluir</button>
+            <button @click="editUser(user)" class="edit-button" title="Editar">
+              <span class="material-icons">edit</span>
+            </button>
+          </td>
+          <td>
+            <button @click="deleteUser(user)" class="delete-button" title="Deletar">
+              <span class="material-icons">delete</span>
+            </button>
           </td>
         </tr>
       </tbody>
